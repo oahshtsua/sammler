ad1c357b4f7dc1538b8003af46fbed573a812567
Aashutosh A. <git.circulate317@passinbox.com>
2025-04-27 13:52:22 +0545

    feat: add migration to enable foreign key constraint

---
+++ migrations/20250427080546_sqlite_enable_fk.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
+PRAGMA foreign_keys = ON;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+PRAGMA foreign_keys = OFF;
+-- +goose StatementEnd
